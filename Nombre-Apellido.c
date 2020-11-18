b39eeff378bb3d83a6ba2e7adb4feea751202069
B13ckp <74647375+B13ckp@users.noreply.github.com>
2020-11-18 11:51:06 -0700

    Create Nombre-Apellido.c

---
+++ Nombre-Apellido.c
@@ -0,0 +1,18 @@
+/* Jhovanny Daniel Beltran Lugo 303 */ 
+
+#include <stdio.h>
+
+int main() {
+	char a[20];
+	char b[20];
+	
+	printf("\nDime tu primer nombre: ");
+  	gets(a);
+	
+	printf("\nDime tu apellido: ");
+  	gets(b);
+	
+  printf("%s %s",b,a);
+  
+	return 0;
+}
